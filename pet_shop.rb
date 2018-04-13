8b7031c28668233f38427467ff60f001ccc6e277
Euan <bell_designs@outlook.com>
2018-04-13 15:25:07 +0100

    pets_by_breed complete

--- pet_shop.rb
+++ pet_shop.rb
@@ -26,3 +26,18 @@ end
 def stock_count(stock)
   stock[:pets].length
 end
+
+def test_all_pets_by_breed__found
+  pets = pets_by_breed(@pet_shop, "British Shorthair")
+  assert_equal(2, pets.count)
+end
+
+def pets_by_breed(shop, breed)
+  i = 0
+  total = Array.new
+  for pet in shop[:pets]
+    total.push(pet) if shop[:pets][i][:breed] == breed
+    i +=1
+  end
+return total
+end
